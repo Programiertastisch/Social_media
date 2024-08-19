0b6de9fb9e82d4e751ead6eaef208ac564a40dc6
Daniel <104966313+dproggen@users.noreply.github.com>
2024-08-19 14:53:26 +0200

    Profil mit Hamburger menü und sidebar hinzugefügt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfilComponent } from './profil/profil.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ProfilComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/profil/profil.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'profil',
+  standalone: true,
+  imports: [],
+  templateUrl: './profil.component.html',
+  styleUrl: './profil.component.css'
+})
+export class ProfilComponent {
+
+}
